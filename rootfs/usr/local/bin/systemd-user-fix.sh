42e0eeae487d963a69d94daf1c5db250d764af0e
D3vil0p3r <crazysnob@live.it>
2023-04-06 02:11:30 +0200

    Update systemd-user-fix.sh

--- rootfs/usr/local/bin/systemd-user-fix.sh
+++ rootfs/usr/local/bin/systemd-user-fix.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
-if [[ "$(systemctl is-active user@1000)" != "active" ]]; then
-    systemctl restart user@1000
+if [[ ! -e "$XDG_RUNTIME_DIR/bus" ]]; then
+    systemctl stop user@1000
+    systemctl start user@1000
 fi
